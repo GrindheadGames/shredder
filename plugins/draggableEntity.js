7cb67dfc363228c6a0ac18920677319741a00419
Craig Beswetherick <craig@grindheadgames.com>
2014-02-21 17:18:37 +0000

    updated dragable entity

--- plugins/draggableEntity.js
+++ plugins/draggableEntity.js
@@ -4,21 +4,38 @@ ig.module('plugins.draggableEntity')
     DraggableEntity = ig.Entity.extend({
 	
 		isDragging: false,
-		clickKey: null,
+		clickKey: '',
+		realXPos:0,
+		realYPos:0,
 
 		update: function()
 		{
+			//use bit wise operators when possible.
+			//we also cache this value here because it is used twice per game loop and we can reduce calculations.
+			this.realXPos = this.pos.x- (this.size.x >> 1);
+			this.realYPos = this.pos.y- (this.size.y >> 1);
+
 			if( this.isDragging )
 			{
-				this.pos.x = ig.input.mouse.x - this.width >> 1;
-				this.pos.y = ig.input.mouse.y - this.height >> 1;
-			}else if( ig.input.pressed(this.clickKey) && this.isPointInsideRect(ig.input.mouse.x, ig.input.mouse.y, this.pos.x, this.pos.y, this.width, this.height))
+				this.pos.x = ig.input.mouse.x;
+				this.pos.y = ig.input.mouse.y;
+			}else if( this.isDragging == false && ig.input.pressed(this.clickKey) && this.isPointInsideRect(ig.input.mouse.x, ig.input.mouse.y,this.realXPos,this.realYPos, this.size.x, this.size.y))
 			{
 				this.isDragging = true;
-			}else if( ig.input.released(this.clickKey))
+			}
+
+			if( ig.input.released(this.clickKey))
 			{
 				this.isDragging = false;
 			}
+
+			this.parent();
+		},
+
+		draw: function()
+		{
+			this.parent();	
+			this.image.draw(this.realXPos, this.realYPos);
 		}
 	});
 });
