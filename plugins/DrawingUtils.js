a36284c6aa5125897e3ea0be5bced4210725aa4a
Luke Wilde <luke.wilde@clock.co.uk>
2014-05-30 10:24:02 +0100

    Adding compatibility to drawing data.

--- plugins/DrawingUtils.js
+++ plugins/DrawingUtils.js
@@ -1,21 +1,27 @@
-ig.module(
-    'plugins.DrawingUtils'
-    )
-.requires(
-  'impact.system'
-  )
-.defines(function () {
-    "use strict";
-  ig.System.inject(drawingData);
-});
 
-var drawingData = {
-  drawColor: function( color, alpha )
-  {
-    var oldAlpha = this.context.globalAlpha;
-    this.context.globalAlpha=alpha;
-    this.context.fillStyle = color;
-    this.context.fillRect( 0, 0, this.realWidth, this.realHeight );
-    this.context.globalAlpha = oldAlpha;
+(function (root) {
+  'use strict';
+
+  var drawingData =
+    { drawColor: function( color, alpha ) {
+        var oldAlpha = this.context.globalAlpha
+        this.context.globalAlpha=alpha
+        this.context.fillStyle = color
+        this.context.fillRect( 0, 0, this.realWidth, this.realHeight )
+        this.context.globalAlpha = oldAlpha
+      }
+    }
+
+  if (typeof ig !== 'undefined') {
+    // Impact.js:
+    ig.module('plugins.DrawingUtils').requires('impact.system').defines(function () {
+      ig.System.inject(drawingData)
+    })
+  } else if (typeof module !== 'undefined' && module.exports) {
+    // CommonJS
+    module.exports = drawingData
+  } else {
+    // Script tag
+    root.drawingData = drawingData
   }
-};
+}(root))
